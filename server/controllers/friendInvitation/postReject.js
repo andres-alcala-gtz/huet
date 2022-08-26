0f8db716cc5224b21a4b0c5844cc01b13675ca58
andres-alcala-gtz <hector.alcala2936@alumnos.udg.mx>
2022-08-26 15:18:38 -0500

    real-time communication

---
+++ server/controllers/friendInvitation/postReject.js
@@ -0,0 +1,23 @@
+const FriendInvitation = require("../../models/friendInvitation");
+const friendsUpdates = require("../../socketHandlers/updates/friends");
+
+const postReject = async (req, res) => {
+  try {
+    const { id } = req.body;
+    const { userId } = req.user;
+
+    const invitationExists = await FriendInvitation.exists({ _id: id });
+
+    if (invitationExists) {
+      await FriendInvitation.findByIdAndDelete(id);
+    }
+
+    friendsUpdates.updateFriendsPendingInvitations(userId);
+
+    return res.status(200).send("Invitation succesfully rejected");
+  } catch (err) {
+    return res.status(500).send("Something went wrong please try again");
+  }
+};
+
+module.exports = postReject;
---
+++ server/middleware/authSocket.js
@@ -0,0 +1,17 @@
+const jwt = require("jsonwebtoken");
+
+const verifyTokenSocket = (socket, next) => {
+  const token = socket.handshake.auth?.token;
+
+  try {
+    const decoded = jwt.verify(token, process.env.TOKEN_KEY);
+    socket.user = decoded;
+  } catch (err) {
+    const socketError = new Error("NOT_AUTHORIZED");
+    return next(socketError);
+  }
+
+  next();
+};
+
+module.exports = verifyTokenSocket;
